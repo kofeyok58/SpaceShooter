3e6949924a387327dd04b4632f0b54591a3647e5
kofeyok58 <kofeyok58@gmail.com>
2025-10-28 13:56:56 +1000

    first commit

---
+++ src/main/java/Keys.java
@@ -0,0 +1,20 @@
+import javafx.scene.Scene;
+import javafx.scene.input.KeyCode;
+
+import java.util.HashSet;
+import java.util.Set;
+
+/** Отслеживание нажатых клавиш */
+public final class Keys {
+    private final Set<KeyCode> down = new HashSet<>();
+
+    public void attach(Scene scene){
+        scene.setOnKeyPressed(e->down.add(e.getCode()));
+
+        scene.setOnKeyReleased(e-> down.remove(e.getCode()));
+
+    }
+
+    public boolean isDown(KeyCode code){return down.contains(code);}
+
+}
